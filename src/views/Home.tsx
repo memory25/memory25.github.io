aeffa2024271900a1cc865dcf0b154661124d4c9
Syuan <as88007@yahoo.com.tw>
2019-01-23 22:18:34 +0800

    [home] update mine picture and base info [images]

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,5 +5,10 @@ export default () => (
     <i className="star-1" />
     <i className="star-2" />
     <i className="star-3" />
+    <div className="main">
+      <div className="me" />
+      <h1 className="name">Sam Jhuang</h1>
+      <h2 className="role">Front-End Developer</h2>
+    </div>
   </div>
 );
