566e62f7f68421d753a5994678de8753c46de786
Sam <102460@senao.com>
2019-02-08 23:25:13 +0800

    [contact] update formsmarts iframe

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
 
-export default () => <div id="contact">contact</div>;
+export default () => (
+  <div id="contact">
+    <div className="title">Contact</div>
+    <div className="main">
+      {/* <!--<iframe class="fs_embed" src="http://formsmarts.com/form/1w4c?mode=h5embed&lay=2" allowfullscreen="true"></iframe>--> */}
+      <iframe
+        title="contact"
+        className="fs_embed"
+        src="https://formsmarts.com/form/1w4c?mode=h5embed&lay=2"
+        allowFullScreen
+      >
+        <a href="http://formsmarts.com/form/1w4c?mode=h5">
+          Can&#39;t see the form? Click here
+        </a>
+        .
+      </iframe>
+      {/* <!-- Do not change or remove this notice. See https://formsmarts.com/warn --> */}
+    </div>
+  </div>
+);
