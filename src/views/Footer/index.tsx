e089d91a16f76fe1c682b2e13bf94e8a2e5a82d9
Sam <102460@senao.com>
2019-02-22 22:48:02 +0800

    finetune footer

--- src/views/Footer/index.tsx
+++ src/views/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
   <div id="footer">
-    Copyright 2019 Syuan | ContactForm by
+    Copyright 2019 Syuan | ContactForm by{' '}
     <a href="https://formsmarts.com/">FormSmarts</a>
   </div>
 );
